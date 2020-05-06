f05dbef7501bc0cdeac780d1125ef3f48c3ec788
AlexHettle <51217513+AlexHettle@users.noreply.github.com>
2020-05-05 21:19:48 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,59 @@
+from tkinter import *
+def initiate():
+    A=EntryA.get()
+    ArrayA=[x.strip() for x in A.split(',')]
+    B=EntryB.get()
+    ArrayB=[x.strip() for x in B.split(',')]
+    EntryA.delete(0,END)
+    EntryB.delete(0,END)
+    AND(ArrayA,ArrayB)
+    OR(ArrayA,ArrayB)
+    XOR(ArrayA,ArrayB)
+def AND(A,B):
+    result=[]
+    for i in A:
+        if i in B and i not in result:
+            result.append(i)
+    result.sort()
+    LabelAND.configure(text=("A AND B: "+", ".join(result)))
+def OR(A,B):
+    result=[]
+    for i in A:
+        if i not in result:
+            result.append(i)
+    for i in B:
+        if i not in result:
+            result.append(i)
+    LabelOR.configure(text=("A OR B: "+", ".join(result)))
+def XOR(A,B):
+    result=[]
+    for i in A:
+        if i not in B and i not in result:
+            result.append(i)
+    for i in B:
+        if i not in A and i not in result:
+            result.append(i)
+    LabelXOR.configure(text=("A XOR B: "+", ".join(result)))
+window=Tk()
+window.title("Subsets")
+window.geometry("800x450")
+window.configure(bg="#bea4e9")
+EntryA=Entry(window,width=50,borderwidth=10)
+EntryA.place(x=450,y=200)
+EntryB=Entry(window,width=50,borderwidth=10)
+EntryB.place(x=450,y=250)
+InfoLabel=Label(text="please enter both sets in a comma format without parenthesis,\n an example would be... \nA: 1,A,3  B: 4,5,Z",font=("fixedsys",10),wraplength=300,bg="#bea4e9")
+InfoLabel.place(x=475,y=350)
+LabelA=Label(text="A:",font=("fixedsys",20),bg="#bea4e9")
+LabelA.place(x=400,y=200)
+LabelB=Label(text="B:",font=("fixedsys",20),bg="#bea4e9")
+LabelB.place(x=400,y=250)
+LabelOR=Label(text="A OR B: ",font=("fixedsys",20),wraplength=400, anchor="w",bg="#bea4e9")
+LabelOR.place(x=0,y=0)
+LabelAND=Label(text="A AND B: ",font=("fixedsys",20),wraplength=400, anchor="w",bg="#bea4e9")
+LabelAND.place(x=0,y=150)
+LabelXOR=Label(text="A XOR B: ",font=("fixedsys",20),wraplength=400, anchor="w",bg="#bea4e9")
+LabelXOR.place(x=0,y=300)
+The_Button=Button(window,text="Enter",padx=136,bg="#CDFFFF",font=("fixedsys",3),command=initiate,)
+The_Button.place(x=450,y=300)
+window.mainloop()
